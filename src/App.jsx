0461db1efbe172d95b4d38a6baa167d50243bd4c
Lijazilla <alejandro-1986-jf@hotmail.com>
2025-02-12 12:02:06 -0600

    Pruebas de HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+const Home = () => <h2>Página Principal</h2>;
+const Page1 = () => <h2>Componente 1</h2>;
+const Page2 = () => <h2>Componente 2</h2>;
+const Page3 = () => <h2>Componente 3</h2>;
+const Page4 = () => <h2>Componente 4</h2>;
+const Page5 = () => <h2>Componente 5</h2>;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div style={{ textAlign: "center", padding: 20 }}>
+        <h1>Prueba de HashRouter</h1>
+        <nav>
+          <Link to="/">Inicio</Link> | 
+          <Link to="/page1"> Página 1</Link> | 
+          <Link to="/page2"> Página 2</Link> | 
+          <Link to="/page3"> Página 3</Link> | 
+          <Link to="/page4"> Página 4</Link> | 
+          <Link to="/page5"> Página 5</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+          <Route path="/page4" element={<Page4 />} />
+          <Route path="/page5" element={<Page5 />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
